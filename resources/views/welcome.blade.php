7f84909ec82d71e4bfa44effb7280c40e65def60
Arrifat <arifatdhivanahara916@gmail.com>
2025-08-14 17:38:04 +0700

    Add inline CSS for testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,6 +30,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <style>
+        body {
+            background-color: #f0f8ff;
+        }
+
+        h1 {
+            color: red;
+        }
+
         .navbar-nav .nav-link {
             color: #343a40;
             /* default gelap */
